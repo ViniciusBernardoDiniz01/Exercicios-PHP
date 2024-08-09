cead79dcfbdf5dec624c8302363d4408138702cf
Vinicius <vinicius.diniz.senai404@gmail.com>
2024-08-09 09:18:59 -0300

    Update exercicio 2.php

--- exercicio 2.php
+++ exercicio 2.php
@@ -1,7 +1,8 @@
 <?php
+// Recebe as variaveis
 $salario = 2500;
 $hora_extra = 6;
-
+// usa do if elseif e else para descobrir se o trabalhador tem desconto
 if ($salario > 2000 && $hora_extra > 5){
     echo "Trabalhador recebe um bonus de 10% <br/>";
 }elseif ($salario < 2000 && $hora_extra > 10){
